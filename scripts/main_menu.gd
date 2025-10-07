53e5b0f8f7fd66f0e715d3a5f4eff92aa3ab7429
Hnnrik <pedrohenriquebzzr@gmail.com>
2025-10-06 21:18:55 -0300

    att

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,7 +1,16 @@
 extends Control
 
-func _on_button_iniciar_jogo_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
+@onready var music_player := $musicaTema
+@onready var blood_falling := $bloodFalling
 
+func _ready():
+	await music_player.ready
+	music_player.playing()
+
+func _on_button_iniciar_jogo_pressed() -> void:
+	blood_falling.show()
+	blood_falling.play("blood_falling")
+	await blood_falling.animation_finished
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
 func _on_button_sair_jogo_pressed() -> void:
 	get_tree().quit()
---
+++ scripts/uiActions.gd
@@ -0,0 +1,21 @@
+extends Node
+
+var game_status = ""
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_play_again_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	get_tree().reload_current_scene()
+	
+func _on_return_to_menu_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
